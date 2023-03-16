736102acdbf53fe46f04c9ee9ce12445501508a6
prettyboy1234 <18324970819@163.com>
2023-03-16 15:00:21 +0800

    unit2_book.c
    书上第二章程序示例

---
+++ cmake-build-debug/CMakeFiles/c_primer_plus6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_primer_plus6.dir/main.c.obj"
+  "c_primer_plus6.exe"
+  "c_primer_plus6.exe.manifest"
+  "c_primer_plus6.pdb"
+  "libc_primer_plus6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_primer_plus6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
